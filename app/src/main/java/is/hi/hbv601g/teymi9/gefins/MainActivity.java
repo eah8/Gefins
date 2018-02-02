2471e2428ca12bc6e3b157af6b585b3416003a22
Einar Andreas Helgason <eah8@hi.is>
2018-02-02 18:50:20 +0000

    Initial commit

---
+++ app/src/main/java/is/hi/hbv601g/teymi9/gefins/MainActivity.java
@@ -0,0 +1,13 @@
+package is.hi.hbv601g.teymi9.gefins;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
